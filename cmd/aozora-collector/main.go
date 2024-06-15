5d75b2c1a13792a7e7844780ee45b3ddb0d5b1dd
Tocyuki <tocguitar1@gmail.com>
2024-06-16 00:09:24 +0900

    作者とZIPファイルのURLを得る関数の追加

--- cmd/aozora-collector/main.go
+++ cmd/aozora-collector/main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"regexp"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
@@ -37,9 +38,34 @@ func findEntries(siteURL string) ([]Entry, error) {
 			return
 		}
 		pageURL := fmt.Sprintf("https://www.aozora.gr.jp/cards/%s/card%s.html", token[1], token[2])
-		println(pageURL)
+		_, zipURL := findAuthorAndZIP(pageURL)
+		println(zipURL)
 	})
 	return nil, nil
+}
+
+func findAuthorAndZIP(siteURL string) (string, string) {
+	res, err := http.Get(siteURL)
+	if err != nil {
+		return "", ""
+	}
+	defer res.Body.Close()
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		return "", ""
+	}
+
+	author := doc.Find("table[summary=作家データ] tr:nth-child(1) td:nth-child(2)").Text()
+
+	zipURL := ""
+	doc.Find("table.download a").Each(func(n int, elem *goquery.Selection) {
+		href := elem.AttrOr("href", "")
+		if strings.HasSuffix(href, ".zip") {
+			zipURL = href
+		}
+	})
+	return author, zipURL
 }
 
 func main() {
