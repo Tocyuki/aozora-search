36b12613fa68b0c57f8efe855a4ede501c69a822
Tocyuki <tocguitar1@gmail.com>
2024-06-28 12:58:52 +0900

    aozora-searchにshowAuthors関数を実装

--- cmd/aozora-search/main.go
+++ cmd/aozora-search/main.go
@@ -10,6 +10,32 @@ import (
 	_ "github.com/mattn/go-sqlite3"
 )
 
+func showAuthors(db *sql.DB) error {
+	rows, err := db.Query(`
+	SELECT
+		a.author_id,
+		a.author
+	FROM
+		authors a
+	ORDER BY
+		CAST(a.author_id AS INTEGER)
+	`)
+	if err != nil {
+		return err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var authorID, author string
+		err = rows.Scan(&authorID, &author)
+		if err != nil {
+			return err
+		}
+		fmt.Printf("%s: %s\n", authorID, author)
+	}
+	return nil
+}
+
 const usage = `
 Usage of ./aozora-search [sub-command] [...]:
   -d string
@@ -40,4 +66,13 @@ func main() {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	switch flag.Arg(0) {
+	case "authors":
+		err = showAuthors(db)
+	}
+
+	if err != nil {
+		log.Fatal(err)
+	}
 }
